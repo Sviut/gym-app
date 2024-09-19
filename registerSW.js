49a4fa3bf139fdd158ee7d0f6df8f1f1ea0ff4ea
Sviut <jeleznov88@gmail.com>
2024-09-19 21:40:58 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://sviut.github.io/gym-appsw.js', { scope: 'https://sviut.github.io/gym-app' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
