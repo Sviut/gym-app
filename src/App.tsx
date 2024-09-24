509302222eef8329b71154120b555a68c5031e0e
Sviut <jeleznov88@gmail.com>
2024-09-24 21:07:13 +0300

    update root

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
 function App() {
-  return <div>Hello World!</div>
+  return (
+    <div className={'h-screen scroll-auto'}>
+      <Outlet />
+    </div>
+  )
 }
 
 export default App
