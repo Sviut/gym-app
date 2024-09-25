76efa210fb511b7c14f43c4a2423316851ca865e
Sviut <jeleznov88@gmail.com>
2024-09-25 11:08:19 +0300

    update counter & workout page

--- src/components/exercise-counter.tsx
+++ src/components/exercise-counter.tsx
@@ -6,33 +6,35 @@ import { Input } from '@/components/ui/input.tsx'
 
 interface ExerciseCounterProps {
   title: string
-  initialValue: number
+  initialValue?: number
+  step?: number
 }
 
 export const ExerciseCounter: FC<ExerciseCounterProps> = ({
   title,
   initialValue = 0,
+  step = 1,
 }) => {
   const [counter, setCounter] = useState<number>(initialValue)
 
-  const incrementCounter = () => setCounter((prevState) => (prevState += 0.5))
-  const decrementCounter = () => setCounter((prevState) => (prevState -= 0.5))
+  const incrementCounter = () => setCounter((prevState) => (prevState += step))
+  const decrementCounter = () => setCounter((prevState) => (prevState -= step))
 
   return (
     <div className={'inline-flex flex-col'}>
       <div className={'text-center text-xs text-gray-400'}>{title}</div>
-      <Card className={'flex w-[80px] flex-col gap-2 p-2'}>
-        <Button variant={'outline'} onClick={incrementCounter}>
-          <ArrowUp />
+      <Card className={'flex gap-2 p-2'}>
+        <Button variant={'ghost'} size={'icon'} onClick={decrementCounter}>
+          <ArrowDown />
         </Button>
         <Input
           inputMode='numeric'
-          className={'text-center font-bold'}
+          className={'text-center'}
           value={counter}
           onChange={(value) => setCounter(+value.target.value)}
         />
-        <Button variant={'outline'} onClick={decrementCounter}>
-          <ArrowDown />
+        <Button variant={'ghost'} size={'icon'} onClick={incrementCounter}>
+          <ArrowUp />
         </Button>
       </Card>
     </div>
--- src/pages/workout.tsx
+++ src/pages/workout.tsx
@@ -1,37 +1,48 @@
 import { ExerciseCounter } from '@/components/exercise-counter.tsx'
 import { Card } from '@/components/ui/card.tsx'
+import { Button } from '@/components/ui/button.tsx'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
 import { Separator } from '@/components/ui/separator.tsx'
 
 const Workout = () => {
   return (
     <div className={'flex flex-col gap-4 p-2'}>
-      <div>Название упражнения</div>
+      <div className={'flex items-center justify-between'}>
+        <Button variant={'ghost'}>
+          <ArrowLeft />
+        </Button>
+        <div>Название упражнения</div>
+        <Button variant={'ghost'}>
+          <ArrowRight />
+        </Button>
+      </div>
 
-      <Card className={'p-4'}>
-        <div className={'mr-6 inline-flex flex-col items-center'}>
-          <div className={'font-bold'}>100</div>
-          <Separator />
-          <div className={'font-bold'}>10</div>
-        </div>
+      <div className={'flex flex-wrap gap-4'}>
+        {new Array(5).fill('str').map((_, i) => (
+          <Card className={'flex min-h-16 min-w-24 items-center p-2'}>
+            {i + 1} <Separator orientation={'vertical'} className={'mx-1'} />
+            <div className={'flex flex-col justify-between'}>
+              <div className={'flex text-xs'}>
+                <div>100</div>x<div>10</div>
+              </div>
 
-        <div className={'mr-6 inline-flex flex-col items-center'}>
-          <div className={'font-bold'}>100</div>
-          <Separator />
-          <div className={'font-bold'}>10</div>
-        </div>
-
-        <div className={'mr-6 inline-flex flex-col items-center'}>
-          <div className={'font-bold'}>100</div>
-          <Separator />
-          <div className={'font-bold'}>10</div>
-        </div>
-      </Card>
+              <div className={'flex font-bold'}>
+                <div>100</div>x<div>10</div>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
 
       <div className={'flex gap-4'}>
-        <ExerciseCounter title={'вес'} initialValue={100} />
+        <ExerciseCounter title={'вес'} initialValue={undefined} />
 
         <ExerciseCounter title={'повторы'} initialValue={10} />
       </div>
+
+      <Button className={'mt-2 w-full'} size={'lg'} variant={'outline'}>
+        Добавить
+      </Button>
     </div>
   )
 }
